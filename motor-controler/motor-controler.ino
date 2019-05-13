de9e339cb1ef55cf206bd33af1c61300663e87e4
Will Welker <wwelker@gmail.com>
2019-05-13 05:44:02 -0700

    autosync base roation

--- motor-controler/motor-controler.ino
+++ motor-controler/motor-controler.ino
@@ -1,10 +1,10 @@
 // https://electronicshobbyists.com/controlling-dc-motors-arduino-arduino-l298n-tutorial/
 
 //Joystick Pins
-int x_key = A0;
-int y_key = A1;
-int x_pos;
-int y_pos;
+int pot1a = A0;
+int pot1b = A1;
+int pos_a1;
+int pos_a2;
 //Motor Pins
 int EN_A = 11;      //Enable pin for first motor
 int IN1 = 9;       //control pin for first motor
@@ -13,8 +13,8 @@ int IN3 = 7;        //control pin for second motor
 int IN4 = 6;        //control pin for second motor
 int EN_B = 10;      //Enable pin for second motor
 //Initializing variables to store data
-int motor_speed;
-int motor_speed1;
+int baseSpeed;
+int shouldSpeed;
 void setup ( ) {
   Serial.begin (9600); //Starting the serial communication at 9600 baud rate
   //Initializing the motor pins as output
@@ -24,51 +24,58 @@ void setup ( ) {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   pinMode(EN_B, OUTPUT);
-  //Initializng the joystick pins as input
-  pinMode (x_key, INPUT) ;
-  pinMode (y_key, INPUT) ;
+  //Initializng the pot pins as input
+  pinMode (pot1a, INPUT) ;
+  pinMode (pot1b, INPUT) ;
 }
 void loop () {
-  x_pos = analogRead (x_key) ;  //Reading the horizontal movement value
-  y_pos = analogRead (y_key) ;  //Reading the vertical movement value
+  pos_a1 = analogRead (pot1a) ;  //Reading input value
+  pos_a2 = analogRead (pot1b) ;  //Reading robot position value
+  //Serial.println(pos_a2);
+  // BASE MOTOR
 
-// MOTOR 1
+  if (pos_a1 > pos_a2) { //CCW
+    baseSpeed = pos_a1 - pos_a2;
+    baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
+    baseSpeed = constrain(baseSpeed, 0, 255);
+    //Serial.println(baseSpeed);
+    if (pos_a2 < 900) {
+      Serial.println(pos_a2);
+      digitalWrite(IN1, LOW); //CCW
+      digitalWrite(IN2, HIGH);
+      analogWrite(EN_A, baseSpeed);
+    } else {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+    }
 
-  if (x_pos < 400) {    //Rotating the left motor in clockwise direction
-    motor_speed = map(x_pos, 400, 0, 0, 255);   //Mapping the values to 0-255 to move the motor
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    analogWrite(EN_A, motor_speed);
   }
-  else if (x_pos > 400 && x_pos < 600) { //Motors will not move when the joystick will be at center
+  if (pos_a1 < pos_a2) { //CW
+    baseSpeed = pos_a2 - pos_a1;
+    baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
+    baseSpeed = constrain(baseSpeed, 0, 255);
+    //Serial.println(baseSpeed);
+    if (pos_a2 > 90) {
+      //Serial.println(pos_a2);
+      digitalWrite(IN1, HIGH); //CW
+      digitalWrite(IN2, LOW);
+      analogWrite(EN_A, baseSpeed);
+    } else {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+    }
+
+  }
+  if (baseSpeed < 10) {
+    baseSpeed = 0;
+    //Serial.println("stop");
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
   }
 
-  else if (x_pos > 600) {   //Rotating the left motor in anticlockwise direction
-    motor_speed = map(x_pos, 600, 1023, 0, 255);
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    analogWrite(EN_A, motor_speed);
-  }
 
-// MOTOR 2
 
-  if (y_pos < 400) {        //Rotating the right motor in clockwise direction
-    motor_speed1 = map(y_pos, 400, 0, 0, 255);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-    analogWrite(EN_B, motor_speed1);
-  }
-  else if (y_pos > 400 && y_pos < 600) {
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
-  }
+  // MOTOR 2
 
-  else if (y_pos > 600) {       //Rotating the right motor in anticlockwise direction
-    motor_speed1 = map(y_pos, 600, 1023, 0, 255);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    analogWrite(EN_B, motor_speed1);
-  }
+
 }
