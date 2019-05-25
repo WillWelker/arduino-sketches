08c242cdea7be9b3d78b631d1a639bb2c529aa47
Will Welker <wwelker@gmail.com>
2019-05-25 04:14:56 -0700

    autosync motors disabled debug ready

--- motor-controler/edit-file.cs
+++ motor-controler/edit-file.cs
@@ -1,44 +1,25 @@
 //I use this CS file to copy/paste bits of code into for more advanced Sublime Text abilties
 
-//  ***Base Operation***  //
-  base_pos = analogRead (pot_6);
-  servo_yz = map(servo_yz, -20, 20, 90, 120);
-  if (servo_yz > base_pos) { //UP
-    baseSpeed = servo_yz - base_pos;
-    baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
-    baseSpeed = constrain(baseSpeed, 0, 255);
-    //Serial.println(baseSpeed);
-    if (servo_yz < 900) { //set limit for UP rotation
-      Serial.println(servo_yz);
-      digitalWrite(basePin1, LOW); //UP
-      digitalWrite(basePin2, HIGH);
-      analogWrite(baseEnable, baseSpeed);
-    } else {
-      digitalWrite(basePin1, LOW);// stop motor
-      digitalWrite(basePin2, LOW);
-    }
+int pot_0 = A0; //hand sensor to control servo claw
+int pot_1 = A1; //not used yet
+int pot_2 = A2; //elbow arm sensor
+int pot_3 = A3; //elbow robot sensor
+int pot_4 = A4; //shoulder robot sensor
+int pot_6 = A6; //base robot sensor
 
-  }
-  if (servo_yz < base_pos) { //DOWN
-    baseSpeed = base_pos - servo_yz;
-    baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
-    baseSpeed = constrain(baseSpeed, 0, 255);
-    //Serial.println(baseSpeed);
-    if (servo_yz > 90) { //set limit for DOWN rotation
-      //Serial.println(base_pos);
-      digitalWrite(basePin1, HIGH); //CW
-      digitalWrite(basePin2, LOW);
-      analogWrite(baseEnable, baseSpeed);
-    } else {
-      digitalWrite(basePin1, LOW); //stop motor
-      digitalWrite(basePin2, LOW);
-    }
+//          Motor Controler Pins
+// elbow motor
+int elboEnable = 11;      //Enable pin for base motor
+int elboPin1 = 9;
+int elboPin2 = 8;
+// shoulder motor
+int shouldEnable = 10;      //Enable pin for shoulder motor
+int shouldPin1 = 7;
+int shouldPin2 = 6;
+// base motor
+int baseEnable = 12;      //Enable pin for elbow motor
+int basePin1 = 14;
+int basePin2 = 15;
 
-  }
-  //if robot position is close enough, stop motor
-  if (baseSpeed < 10) {
-    baseSpeed = 0;
-    //Serial.println("stop");
-    digitalWrite(basePin1, LOW);
-    digitalWrite(basePin2, LOW);
-  }
+int enablePin = 16; // for enable/disable switch
+int disengagePin = 17; // for push button disengage (like lifting up your mouse to reset its position)
--- motor-controler/motor-controler.ino
+++ motor-controler/motor-controler.ino
@@ -1,9 +1,15 @@
 // Athena's Robot Arm Project
+// Running on Arduino Mega
 // Include Wire.h to communicate with MPU-6050 over I2C
 #include <Wire.h>
 // Include Servo.h to run the claw servo
 #include <Servo.h>
 
+//Varables for enable and reset position
+bool enable;
+bool disengage;
+int enablePin = 16; // for enable/disable switch
+int disengagePin = 17; // for push button disengage (like lifting up your mouse to reset its position)
 //Variables for Gyroscope
 int gyro_x, gyro_y, gyro_z;
 long gyro_x_cal, gyro_y_cal, gyro_z_cal;
@@ -23,7 +29,7 @@ Servo clawServo;
 int servoPos;
 //Potentiometer analog input pins a=arm sensor b=robot arm sensor
 int pot_0 = A0; //hand sensor to control servo claw
-int pot_1 = A1;
+int pot_1 = A1; //not used yet
 int pot_2 = A2; //elbow arm sensor
 int pot_3 = A3; //elbow robot sensor
 int pot_4 = A4; //shoulder robot sensor
@@ -74,12 +80,15 @@ void setup ( ) {
   gyro_x_cal /= 1000;
   gyro_y_cal /= 1000;
   gyro_z_cal /= 1000;
-  
+
   Serial.begin (9600); //Starting the serial communication at 9600 baud rate
   // Init Timer
   loop_timer = micros();
   //Initialize the servo pin
   clawServo.attach(28);
+  // enable switch input pin
+  pinMode(enablePin, INPUT_PULLUP);
+  pinMode(disengagePin, INPUT_PULLUP);
   //Initialize the motor control pins as output
   pinMode(elboEnable, OUTPUT);
   pinMode(elboPin1, OUTPUT);
@@ -100,221 +109,221 @@ void setup ( ) {
   pinMode (pot_6, INPUT) ;
 }
 void loop () {
-  // **Get data from MPU-6050** //
-  read_mpu_6050_data();
-  //Subtract the offset values from the raw gyro values
-  gyro_x -= gyro_x_cal;
-  gyro_y -= gyro_y_cal;
-  gyro_z -= gyro_z_cal;
-  //Gyro angle calculations . Note 0.0000611 = 1 / (250Hz x 65.5)
+  enable = digitalRead(enablePin);
+  if (enable = LOW) { // if enable pulled low, run everything
+    // **Get data from MPU-6050** //
+    read_mpu_6050_data();
+    //Subtract the offset values from the raw gyro values
+    gyro_x -= gyro_x_cal;
+    gyro_y -= gyro_y_cal;
+    gyro_z -= gyro_z_cal;
+    //Gyro angle calculations . Note 0.0000611 = 1 / (250Hz x 65.5)
 
-  //Calculate the traveled pitch angle and add this to the angle_pitch variable
-  angle_pitch += gyro_x * 0.0000611;
-  //Calculate the traveled roll angle and add this to the angle_roll variable
-  //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
-  angle_roll += gyro_y * 0.0000611;
-  angle_yaw += gyro_z * 0.0000611;
-  //If the IMU has yawed transfer the roll angle to the pitch angle
-  //angle_pitch += angle_roll * sin(gyro_z * 0.000001066);
-  //If the IMU has yawed transfer the pitch angle to the roll angle
-  //angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);
+    //Calculate the traveled pitch angle and add this to the angle_pitch variable
+    disengage = digitalRead(disengagePin);
+    if (disengage = LOW) { // if disengage button is not pressed, calculate movement
+      angle_pitch += gyro_x * 0.0000611;
+      //Calculate the traveled roll angle and add this to the angle_roll variable
+      //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
+      angle_roll += gyro_y * 0.0000611;
+      angle_yaw += gyro_z * 0.0000611;
+      //If the IMU has yawed transfer the roll angle to the pitch angle
+      //angle_pitch += angle_roll * sin(gyro_z * 0.000001066);
+      //If the IMU has yawed transfer the pitch angle to the roll angle
+      //angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);
 
-  //Calculate the total accelerometer vector
-  acc_total_vector = sqrt((acc_x * acc_x) + (acc_y * acc_y) + (acc_z * acc_z));
+      //Calculate the total accelerometer vector
+      //acc_total_vector = sqrt((acc_x * acc_x) + (acc_y * acc_y) + (acc_z * acc_z));
+    }
+    //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
+    //Calculate the pitch angle
+    //angle_pitch_acc = asin((float)acc_y/acc_total_vector)* 57.296;
+    //Calculate the roll angle
+    //angle_roll_acc = asin((float)acc_x/acc_total_vector)* -57.296;
 
-  //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
-  //Calculate the pitch angle
-  //angle_pitch_acc = asin((float)acc_y/acc_total_vector)* 57.296;
-  //Calculate the roll angle
-  //angle_roll_acc = asin((float)acc_x/acc_total_vector)* -57.296;
+    //Accelerometer calibration value
+    angle_pitch_acc -= 0.0;
+    angle_roll_acc -= 0.0;
+    angle_yaw_acc -= 0.0;
+    if (set_gyro_angles) {
 
-  //Accelerometer calibration value
-  angle_pitch_acc -= 0.0;
-  angle_roll_acc -= 0.0;
-  angle_yaw_acc -= 0.0;
-  if (set_gyro_angles) {
+      //If the IMU has been running
+      //Correct the drift of the gyro pitch angle with the accelerometer pitch angle
+      //angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;
+      //Correct the drift of the gyro roll angle with the accelerometer roll angle
+      //angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;
+    }
+    else {
+      //IMU has just started
+      //Set the gyro pitch/roll/yaw angle equal to the accelerometer pitch/roll/yaw angle
+      angle_pitch = angle_pitch_acc;
+      angle_roll = angle_roll_acc;
+      angle_yaw = angle_yaw_acc;
+      //Set the IMU started flag
+      set_gyro_angles = true;
+    }
 
-    //If the IMU has been running
-    //Correct the drift of the gyro pitch angle with the accelerometer pitch angle
-    //angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;
-    //Correct the drift of the gyro roll angle with the accelerometer roll angle
-    //angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;
-  }
-  else {
-    //IMU has just started
-    //Set the gyro pitch/roll/yaw angle equal to the accelerometer pitch/roll/yaw angle
-    angle_pitch = angle_pitch_acc;
-    angle_roll = angle_roll_acc;
-    angle_yaw = angle_yaw_acc;
-    //Set the IMU started flag
-    set_gyro_angles = true;
-  }
+    //To dampen the pitch and roll angles a complementary filter is used
+    //Take 90% of the output value and add 10% of the raw value
+    angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;
+    angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;
+    angle_yaw_output = angle_yaw_output * 0.9 + angle_yaw * 0.1;
+    //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
 
-  //To dampen the pitch and roll angles a complementary filter is used
-  //Take 90% of the output value and add 10% of the raw value
-  angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;
-  angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;
-  angle_yaw_output = angle_yaw_output * 0.9 + angle_yaw * 0.1;
-  //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
+    // Print to Serial Monitor
 
-  // Print to Serial Monitor
-  
-  servo_x = angle_pitch_output; //int(map(angle_pitch_output, -100, 100, 0, 180));
-  servo_y = angle_roll_output; //int(map(angle_roll_output, 0, 200, 0, 180));
-  servo_z = angle_yaw_output; //int(map(angle_yaw_output, 0, 200, 0, 180));
-  if (servo_x > 0) {
-    servo_yz = servo_y + servo_z;
-  }
-  if (servo_x < 0) {
-    servo_yz = (servo_y * -1) + servo_z;
-  }
-  //Serial.print(" | x = "); Serial.print(servo_x);
-  //Serial.print(" | YZ = "); Serial.println(servo_yz);
-  //Serial.print(" | Z = "); Serial.println(servo_z);
-  // ***END MPU calcs*** //
-  
-  // ***Claw Operation***  //
-  hand_pos = analogRead (pot_0) ;  //Reading input value
-  servoPos = map(hand_pos, 0, 1024, 0, 170);
-  clawServo.write(servoPos);
-  
-  // ***Elbow***  //
-  elbo1_pos = analogRead (pot_2);  //Read elbow arm position value
-  elbo2_pos = analogRead (pot_3);  //Read elbow robot position value
-  if (elbo1_pos > elbo2_pos) { //UP
-    elbowSpeed = elbo1_pos - elbo2_pos;
-    elbowSpeed = map(elbowSpeed, 0, 1024, 0, 3000);
-    elbowSpeed = constrain(elbowSpeed, 0, 255);
-    //Serial.println(elbowSpeed);
-    if (elbo1_pos < 900) { //set limit for UP rotation
-      Serial.println(elbo1_pos);
+    servo_x = angle_pitch_output; //int(map(angle_pitch_output, -100, 100, 0, 180));
+    servo_y = angle_roll_output; //int(map(angle_roll_output, 0, 200, 0, 180));
+    servo_z = angle_yaw_output; //int(map(angle_yaw_output, 0, 200, 0, 180));
+    if (servo_x > 0) {
+      servo_yz = servo_y + servo_z;
+    }
+    if (servo_x < 0) {
+      servo_yz = (servo_y * -1) + servo_z;
+    }
+    //Serial.print(" | x = "); Serial.print(servo_x);
+    //Serial.print(" | YZ = "); Serial.println(servo_yz);
+    //Serial.print(" | Z = "); Serial.println(servo_z);
+    // ***END MPU calcs*** //
+
+    // ***Claw Operation***  //
+    hand_pos = analogRead (pot_0) ;  //Reading input value
+    servoPos = map(hand_pos, 0, 1024, 0, 170);
+    clawServo.write(servoPos);
+
+    // ***Elbow***  //
+    elbo1_pos = analogRead (pot_2);  //Read elbow arm position value
+    elbo2_pos = analogRead (pot_3);  //Read elbow robot position value
+    Serial.print("Elbo position: ");
+    Serial.print(elbo2_pos);
+    if (elbo1_pos > elbo2_pos) { //UP
+      elbowSpeed = elbo1_pos - elbo2_pos;
+      elbowSpeed = map(elbowSpeed, 0, 1024, 0, 3000);
+      elbowSpeed = constrain(elbowSpeed, 0, 255);
+      //Serial.println(elbowSpeed);
       digitalWrite(elboPin1, LOW); //UP
-      digitalWrite(elboPin2, HIGH);
-      analogWrite(elboEnable, elbowSpeed);
-    } else {
-      digitalWrite(elboPin1, LOW);// stop motor
+      if (elbo2_pos < 700) { //set limit for UP rotation
+        digitalWrite(elboPin2, HIGH);
+        //analogWrite(elboEnable, elbowSpeed);
+      } else {
+        digitalWrite(elboPin2, LOW); //stop motor
+      }
+
+    }
+    if (elbo1_pos < elbo2_pos) { //DOWN
+      baseSpeed = elbo2_pos - elbo1_pos;
+      baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
+      baseSpeed = constrain(baseSpeed, 0, 255);
+      //Serial.println(baseSpeed);
+      digitalWrite(elboPin2, LOW);
+      if (elbo2_pos > 200) { //set limit for DOWN rotation
+        //Serial.println(elbo1_pos);
+        digitalWrite(elboPin1, HIGH); //DOWN
+        //analogWrite(elboEnable, elbowSpeed);
+      } else {
+        digitalWrite(elboPin1, LOW); //stop motor
+        
+      }
+
+    }
+    //if robot position is close enough, stop motor
+    if (elbowSpeed < 10) {
+      elbowSpeed = 0;
+      //Serial.println("stop");
+      digitalWrite(elboPin1, LOW);
       digitalWrite(elboPin2, LOW);
     }
 
-  }
-  if (elbo1_pos < elbo2_pos) { //DOWN
-    baseSpeed = elbo2_pos - elbo1_pos;
-    baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
-    baseSpeed = constrain(baseSpeed, 0, 255);
-    //Serial.println(baseSpeed);
-    if (elbo1_pos > 90) { //set limit for DOWN rotation
-      //Serial.println(elbo1_pos);
-      digitalWrite(elboPin1, HIGH); //CW
-      digitalWrite(elboPin2, LOW);
-      analogWrite(elboEnable, elbowSpeed);
-    } else {
-      digitalWrite(elboPin1, LOW); //stop motor
-      digitalWrite(elboPin2, LOW);
-    }
-
-  }
-  //if robot position is close enough, stop motor
-  if (elbowSpeed < 10) {
-    elbowSpeed = 0;
-    //Serial.println("stop");
-    digitalWrite(elboPin1, LOW);
-    digitalWrite(elboPin2, LOW);
-  }
-
-  //  ***Shoulder Operation***  //
-  shoulder_pos = analogRead (pot_4);
-  servo_x = map(servo_x, -20, 20, 90, 120);
-  if (servo_x > shoulder_pos) { //UP
-    shouldSpeed = servo_x - shoulder_pos;
-    shouldSpeed = map(shouldSpeed, 0, 1024, 0, 3000);
-    shouldSpeed = constrain(shouldSpeed, 0, 255);
-    //Serial.println(shouldSpeed);
-    if (servo_x < 900) { //set limit for UP rotation
-      Serial.println(servo_x);
+    //  ***Shoulder Operation***  //
+    shoulder_pos = analogRead (pot_4);
+    Serial.print("| Shoulder position: ");
+    Serial.print(shoulder_pos);
+    servo_x = map(servo_x, -20, 20, 90, 120);
+    if (servo_x > shoulder_pos) { //UP
+      shouldSpeed = servo_x - shoulder_pos;
+      shouldSpeed = map(shouldSpeed, 0, 1024, 0, 3000);
+      shouldSpeed = constrain(shouldSpeed, 0, 255);
+      //Serial.println(shouldSpeed);
       digitalWrite(shouldPin1, LOW); //UP
-      digitalWrite(shouldPin2, HIGH);
-      analogWrite(shouldEnable, shouldSpeed);
-    } else {
-      digitalWrite(shouldPin1, LOW);// stop motor
+      if (servo_x < 900) { //set limit for UP rotation
+        digitalWrite(shouldPin2, HIGH);
+        //analogWrite(shouldEnable, shouldSpeed);
+      } else {
+        digitalWrite(shouldPin2, LOW);
+      }
+
+    }
+    if (servo_x < shoulder_pos) { //DOWN
+      shouldSpeed = shoulder_pos - servo_x;
+      shouldSpeed = map(shouldSpeed, 0, 1024, 0, 3000);
+      shouldSpeed = constrain(shouldSpeed, 0, 255);
+      //Serial.println(baseSpeed);
+      digitalWrite(shouldPin2, LOW);
+      if (servo_x > 90) { //set limit for DOWN rotation
+        digitalWrite(shouldPin1, HIGH); //DOWN
+        //analogWrite(shouldEnable, shouldSpeed);
+      } else {
+        digitalWrite(shouldPin1, LOW); //stop motor
+      }
+
+    }
+    //if robot position is close enough, stop motor
+    if (shouldSpeed < 10) {
+      shouldSpeed = 0;
+      //Serial.println("stop");
+      digitalWrite(shouldPin1, LOW);
       digitalWrite(shouldPin2, LOW);
     }
 
-  }
-  if (servo_x < shoulder_pos) { //DOWN
-    shouldSpeed = shoulder_pos - servo_x;
-    shouldSpeed = map(shouldSpeed, 0, 1024, 0, 3000);
-    shouldSpeed = constrain(shouldSpeed, 0, 255);
-    //Serial.println(baseSpeed);
-    if (servo_x > 90) { //set limit for DOWN rotation
-      //Serial.println(should_pos);
-      digitalWrite(shouldPin1, HIGH); //CW
-      digitalWrite(shouldPin2, LOW);
-      analogWrite(shouldEnable, shouldSpeed);
-    } else {
-      digitalWrite(shouldPin1, LOW); //stop motor
-      digitalWrite(shouldPin2, LOW);
-    }
-
-  }
-  //if robot position is close enough, stop motor
-  if (shouldSpeed < 10) {
-    shouldSpeed = 0;
-    //Serial.println("stop");
-    digitalWrite(shouldPin1, LOW);
-    digitalWrite(shouldPin2, LOW);
-  }
-
-  //  ***Base Operation***  //
-  base_pos = analogRead (pot_6);
-  servo_yz = map(servo_yz, -20, 20, 90, 120);
-  if (servo_yz > base_pos) { //Left
-    baseSpeed = servo_yz - base_pos;
-    baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
-    baseSpeed = constrain(baseSpeed, 0, 255);
-    //Serial.println(baseSpeed);
-    if (servo_yz < 900) { //set limit for Left rotation
-      Serial.println(servo_yz);
+    //  ***Base Operation***  //
+    base_pos = analogRead (pot_6);
+    Serial.print("| Base position: ");
+    Serial.println(base_pos);
+    servo_yz = map(servo_yz, -20, 20, 90, 120);
+    if (servo_yz > base_pos) { //Left
+      baseSpeed = servo_yz - base_pos;
+      baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
+      baseSpeed = constrain(baseSpeed, 0, 255);
+      //Serial.println(baseSpeed);
       digitalWrite(basePin1, LOW); //Left
-      digitalWrite(basePin2, HIGH);
-      analogWrite(baseEnable, baseSpeed);
-    } else {
-      digitalWrite(basePin1, LOW);// stop motor
+      if (servo_yz < 900) { //set limit for Left rotation
+        digitalWrite(basePin2, HIGH);
+        //analogWrite(baseEnable, baseSpeed);
+      } else {
+        digitalWrite(basePin2, LOW); //stop motor
+      }
+
+    }
+    if (servo_yz < base_pos) { //Right
+      baseSpeed = base_pos - servo_yz;
+      baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
+      baseSpeed = constrain(baseSpeed, 0, 255);
+      //Serial.println(baseSpeed);
+      digitalWrite(basePin2, LOW);
+      if (servo_yz > 90) { //set limit for Right rotation
+        digitalWrite(basePin1, HIGH); //Right
+        //analogWrite(baseEnable, baseSpeed);
+      } else {
+        digitalWrite(basePin1, LOW); //stop motor
+      }
+
+    }
+    //if robot position is close enough, stop motor
+    if (baseSpeed < 10) {
+      baseSpeed = 0;
+      //Serial.println("stop");
+      digitalWrite(basePin1, LOW);
       digitalWrite(basePin2, LOW);
     }
 
+
+
+    // The MPU 6050 produces new data at 250HZ
+    // So we pause until enough microseconds have passed
+    while (micros() - loop_timer < 4000);
+    //Reset the loop timer
+    loop_timer = micros();
   }
-  if (servo_yz < base_pos) { //Right
-    baseSpeed = base_pos - servo_yz;
-    baseSpeed = map(baseSpeed, 0, 1024, 0, 3000);
-    baseSpeed = constrain(baseSpeed, 0, 255);
-    //Serial.println(baseSpeed);
-    if (servo_yz > 90) { //set limit for Right rotation
-      //Serial.println(base_pos);
-      digitalWrite(basePin1, HIGH); //Right
-      digitalWrite(basePin2, LOW);
-      analogWrite(baseEnable, baseSpeed);
-    } else {
-      digitalWrite(basePin1, LOW); //stop motor
-      digitalWrite(basePin2, LOW);
-    }
-
-  }
-  //if robot position is close enough, stop motor
-  if (baseSpeed < 10) {
-    baseSpeed = 0;
-    //Serial.println("stop");
-    digitalWrite(basePin1, LOW);
-    digitalWrite(basePin2, LOW);
-  }
-
-
-  
-// The MPU 6050 produces new data at 250HZ
-// So we pause until enough microseconds have passed
-while (micros() - loop_timer < 4000);
-  //Reset the loop timer
-  loop_timer = micros();
-
 }
 // ***End of Loop *** //
 
